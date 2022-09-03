1ffec16f25e2213343fb066176f57c39ad503947
SHERIF Abu El-khair <sherif@SHERIFs-MacBook-Air.local>
2022-09-03 19:31:07 +0200

    sat-2

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a4cf76a956368445994706384502220c-123894222.eu-central-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
